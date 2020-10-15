b8d1f0e2b38113eef7c9841de627764c240ac6df
davilucs <72921790+davilucs@users.noreply.github.com>
2020-10-15 09:15:35 -0300

    Create a Constructor to Vendedor

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -4,6 +4,11 @@
 class Vendedor : public Empregado {
 const double mesPorAno = 12;
   public:  
+	Vendedor(std::string nome; double salarioHora; double quotaMensalVendas){
+		_nome =nome;
+  		_salarioHora = salarioHora;
+  		_quotaMensalVendas = quotaMensalVendas;
+	}
 	double _quotaMensalVendas;
 	double quotaTotalAnual() {
 	  return quotaMensalVendas * mesPorAno;

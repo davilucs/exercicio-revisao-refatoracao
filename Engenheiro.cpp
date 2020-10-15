e8441d2e8a63cc33047b68528d22be664eecc51f
davilucs <72921790+davilucs@users.noreply.github.com>
2020-10-15 09:12:16 -0300

    Create a Constructor to Engenheiro

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -2,9 +2,13 @@
 #include "Empregado.hpp"
 
 class Engenheiro : public Empregado {
-
-  public:
-	int _projetos;
+  	public:
+		Engenheiro(std::string nome; double salarioHora; double int projetos){
+			_nome =nome;
+			_salarioHora = salarioHora;
+			_projetos = projetos;
+		}
+		int _projetos;
 	
 };
 
